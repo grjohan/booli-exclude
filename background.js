863faa6ec5ab1e424995c0815b2daf308394fa71
grjohan <grjohan@student.chalmers.se>
2014-06-12 23:35:15 +0200

    Make program work accordig to readme

--- background.js
+++ background.js
@@ -1,12 +1,11 @@
-var exclusions = {};
+var exclusions = [];
 chrome.runtime.onMessage.addListener(function(msg, sender, response) {
 	if(msg.message === "getExclusions") {
 		response(exclusions);
 	}
-	else if( msg.message ==="addExclusion") {
-		exclusions[msg.exclusion] = msg.exclusion; 
-	}
-	else if( msg.message ==="removeExclusion") {
-		delete exclusions[msg.exclusion];
+	else if( msg.message ==="setExclusion") {
+		exclusions = msg.exclusion;
+		response(exclusions);
+		
 	}
 });
--- booli-exclude.js
+++ booli-exclude.js
@@ -1,22 +1,18 @@
 var exclusions;
+var exclusionString;
 // Send a message to the background with the callback
-chrome.runtime.sendMessage(
-	{message : "getExclusions"},
-    function(respondedExclusions) {
-    	removeExclusions(respondedExclusions)
-  }
- );
 
 function removeExclusions(exclusions) {
 	var query = '[class*=areaCell]';
 	var deleted = false;
     var elems = document.querySelectorAll(query);
       var i;
+      var j;
       for(i = 0; i < elems.length; i++){
       	var str = elems[i].innerHTML.toLowerCase();
       	var j;
-      	for(var exclusion in exclusions)
-  	    if(str.indexOf(exclusions[exclusion]) !== -1) {
+      	for(j = 0; j < exclusions.length; j++)
+  	    if((str.indexOf(exclusions[j]) !== -1) && exclusions[j].trim() !== "" ) {
       		var parent = elems[i];
         	for(var j = 0; j < 3 ; j++){
             	parent = parent.parentNode;
@@ -26,9 +22,39 @@ function removeExclusions(exclusions) {
 	        }
       	}
     }
-    // look at the innerHTML
-	// search for class areaCell
-	//go through them looking for an exclusion
-	//remove it
 }
+
+	var query = '[id*=extendedRefine]';
+    var elems = document.querySelectorAll(query);
+    var newElem = document.createElement('div');
+    newElem.className="listings col1";
+    newElem.innerHTML = "<strong>Excludera områden:</strong> <input type='text' id='extensionInput'></input>";
+    elems[0].appendChild(newElem);
+
+
+	var query = '[class*=showHits]';
+    var elems = document.querySelectorAll(query);
+    var query = '[id*=extensionInput]';
+   	var inputElem = document.querySelectorAll(query);
+    elems[0].onclick = function() {
+    	exclusionString = inputElem[0].value;
+    	var partsOfStr = exclusionString.split(',');
+    	var i;
+    	for(i = 0; i< partsOfStr.length ; i++) {
+    		partsOfStr[i] = partsOfStr[i].trim();
+    	}
+    	chrome.runtime.sendMessage({
+   		message : "setExclusion",
+		exclusion : partsOfStr}
+		);
+		removeExclusions(partsOfStr);
+    };
+    
+	
+ chrome.runtime.sendMessage(
+	{message : "getExclusions"},
+    function(respondedExclusions) {
+    	removeExclusions(respondedExclusions);
+  }
+ );
 // in the callback go through the DOM and remove all objects that should be removed.
