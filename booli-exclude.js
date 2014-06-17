b77103c64691cb665f236fef44beff580e387cae
grjohan <grjohan@student.chalmers.se>
2014-06-17 11:49:11 +0200

    make code prettier

--- booli-exclude.js
+++ booli-exclude.js
@@ -1,16 +1,10 @@
-var exclusions;
-var exclusionString;
-// Send a message to the background with the callback
-
+// Remove hits that match any exclusions
 function removeExclusions(exclusions) {
 	var query = '[class*=areaCell]';
-	var deleted = false;
     var elems = document.querySelectorAll(query);
-      var i;
-      var j;
+      var i,j;
       for(i = 0; i < elems.length; i++){
       	var str = elems[i].innerHTML.toLowerCase();
-      	var j;
       	for(j = 0; j < exclusions.length; j++)
   	    if((str.indexOf(exclusions[j]) !== -1) && exclusions[j].trim() !== "" ) {
       		var parent = elems[i];
@@ -23,41 +17,48 @@ function removeExclusions(exclusions) {
       	}
     }
 }
-	var query = '[id*=refineBox]';
-    var elems = document.querySelectorAll(query);
-    var newElem = document.createElement('div');
-    newElem.className="col1";
-    newElem.innerHTML = "<strong>Excludera områden:</strong>";
-    elems[0].appendChild(newElem);
-    var inputElem = document.createElement('input');
-    inputElem.type="text";
-    inputElem.name="exclusions";
-    inputElem.className="autocomplete";
-    var tempExclusionString = localStorage["booli.exclusions"];
-    if(tempExclusionString !== undefined){
-        var i;
-        var partsOfStr = tempExclusionString.split(',');
-        for(i = 0; i< partsOfStr.length ; i++) {
-        partsOfStr[i] = partsOfStr[i].trim();
-        removeExclusions(partsOfStr);
-      }
+
+// Get the parent for the exclusion element
+var query = '[id*=refineBox]';
+var elems = document.querySelectorAll(query);
+var inputElem = addInputChild(elems[0]);
+// If there are already exclusions in local storage, exclude them
+var tempExclusionString = localStorage["booli.exclusions"];
+if(tempExclusionString !== undefined){
+    var i;
+    var partsOfStr = tempExclusionString.split(',');
+    for(i = 0; i< partsOfStr.length ; i++) {
+      partsOfStr[i] = partsOfStr[i].trim();
+      removeExclusions(partsOfStr);
     }
-    newElem.appendChild(inputElem);
+} 
 
+// Add onclick to the button
+var query = '[class*=showHits]';
+var elems = document.querySelectorAll(query);
+elems[0].onclick = function() {
+  exclusionString = inputElem.value;
+  localStorage["booli.exclusions"] = exclusionString; 
+  var partsOfStr = exclusionString.split(',');
+  var i;
+  for(i = 0; i< partsOfStr.length ; i++) {
+  	partsOfStr[i] = partsOfStr[i].trim();
+  }
+  removeExclusions(partsOfStr);
+};
 
-	var query = '[class*=showHits]';
-    var elems = document.querySelectorAll(query);
-    elems[0].onclick = function() {
-    	exclusionString = inputElem.value;
-      localStorage["booli.exclusions"] = exclusionString; 
-    	var partsOfStr = exclusionString.split(',');
-    	var i;
-    	for(i = 0; i< partsOfStr.length ; i++) {
-    		partsOfStr[i] = partsOfStr[i].trim();
-    	}
-		removeExclusions(partsOfStr);
-    };
-    
- var query = '[class*=toggleRefine]';
- var elems = document.querySelectorAll(query);
+// add the inputElement and text
+function addInputChild(parent){
+  var newElem = document.createElement('div');
+  newElem.className="col1";
+  newElem.innerHTML = "<strong>Excludera områden:</strong>";
+  parent.appendChild(newElem);
+  var childElement = document.createElement('input');
+  newElem.appendChild(childElement);
+  childElement.type="text";
+  childElement.name="exclusions";
+  childElement.className="autocomplete";
+  childElement.placeholder = "Exclusions";
+  return childElement;
+}    
 
